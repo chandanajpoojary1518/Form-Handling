cac296792a8211b35e1a4e863cf3181e3c852d94
chandanajpoojary1518 <chandanajpoojary1518@gmail.com>
2025-11-10 12:08:04 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, session, redirect,url_for #rediect -> open any page to open
+               #url_for function
+app = Flask(__name__)
+users=[] #list
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method =='POST':                 #to check if user types in fortend or not
+        email =request.form['email']
+        password =request.form['password'] 
+            
+        for user in users:
+               if(user['email']==email):
+                  return 'User already exists!!'
+        users.append({'email':email,'password':password}) 
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method =='POST':
+        email =request.form['email']
+        password =request.form['password']
+
+        for user in users:
+            if(user['email']==email and user['password']==password):
+                return render_template('home.html',email=email)
+            
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
